feb9aa4a47a38b436f8672f4c6d358bb436ffaf0
Patryk Blazinski <blazinski.patryk@gmail.com>
2020-12-05 13:17:51 +0100

    refactor: 💡 Format code

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -23,6 +23,9 @@
     font-weight: bold;
     font-size: 40px;
     backface-visibility: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   .card__face--front {
@@ -35,6 +38,14 @@
 </style>
 
 <script lang="ts">
+  import Bear from '../svg/Bear.svelte';
+  import Bunny from '../svg/Bunny.svelte';
+  import Donkey from '../svg/Donkey.svelte';
+  import Duck from '../svg/Duck.svelte';
+  import Elephant from '../svg/Elephant.svelte';
+  import Lion from '../svg/Lion.svelte';
+  import Pig from '../svg/Pig.svelte';
+  import Reindeer from '../svg/Reindeer.svelte';
   import type { Card } from '../interfaces/card';
 
   export let card: Card;
@@ -43,5 +54,30 @@
 
 <div class="card" class:card--flipped="{card.filpped}" on:click="{cardClickHandler}">
   <div class="card__face card__face--front"></div>
-  <div class="card__face card__face--back" style="background: {card.color}"></div>
+  <div class="card__face card__face--back" style="background: white">
+    {#if card.backgroundSvg === 'Bear'}
+      <Bear />
+    {/if}
+    {#if card.backgroundSvg === 'Bunny'}
+      <Bunny />
+    {/if}
+    {#if card.backgroundSvg === 'Donkey'}
+      <Donkey />
+    {/if}
+    {#if card.backgroundSvg === 'Duck'}
+      <Duck />
+    {/if}
+    {#if card.backgroundSvg === 'Elephant'}
+      <Elephant />
+    {/if}
+    {#if card.backgroundSvg === 'Lion'}
+      <Lion />
+    {/if}
+    {#if card.backgroundSvg === 'Pig'}
+      <Pig />
+    {/if}
+    {#if card.backgroundSvg === 'Reindeer'}
+      <Reindeer />
+    {/if}
+  </div>
 </div>
